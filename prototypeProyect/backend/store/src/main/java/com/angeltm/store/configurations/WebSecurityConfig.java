a83151c8fe4d1268ea2a81798915ad4c3917550e
AngelTM <angel1997amg@gmail.com>
2023-03-10 10:53:19 -0700

    change in packages

---
+++ prototypeProyect/backend/store/src/main/java/com/angeltm/store/configurations/WebSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.angeltm.store.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig  {
+    
+}
