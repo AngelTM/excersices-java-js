54c5e1ead2cb9fd0144023a95663da66005e5ac0
AngelTM <angel1997amg@gmail.com>
2023-03-13 13:47:10 -0700

    included usersComponent

--- prototypeProyect/frontend/store-front/src/App.js
+++ prototypeProyect/frontend/store-front/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
+import  UsersComponent  from './components/UsersComponent';
 import { NavbarComponent } from './components/NavbarComponent';
+import  SearchComponent  from './components/SearchComponent';
 
 function App() {
   return (
@@ -9,6 +11,12 @@ function App() {
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
         <div>
           <NavbarComponent/>
+          <div className='container'>
+            <br/>
+            <SearchComponent/>
+            <br/>
+            <UsersComponent/>
+          </div>
         </div>
     </div>
   );
--- prototypeProyect/frontend/store-front/src/components/NavbarComponent.jsx
+++ prototypeProyect/frontend/store-front/src/components/NavbarComponent.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 export const NavbarComponent = () => {
   return (
         <div>
-            <header class="d-flex justify-content-center py-3 bg-light">
-            <ul class="nav nav-pills">
-                <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Users</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Products</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+            <header className="d-flex justify-content-center py-3 bg-light">
+            <ul className="nav nav-pills">
+                <li className="nav-item"><a href="#" className="nav-link active" aria-current="page">Home</a></li>
+                <li className="nav-item"><a href="#" className="nav-link">Users</a></li>
+                <li className="nav-item"><a href="#" className="nav-link">Products</a></li>
+                <li className="nav-item"><a href="#" className="nav-link">About</a></li>
             </ul>
             </header>
         </div>
